18c675d40af7a1a525331f2b9f6a059258f52b99
Uyi Oronsaye <uoronsaye@gmail.com>
2025-02-08 14:07:19 -0500

    Added some highlighs to some loyola locations

--- myApp/app/utils/mapUtils.tsx
+++ myApp/app/utils/mapUtils.tsx
@@ -20,7 +20,7 @@ export const buildings: Building[] = [
       { latitude: 45.497708764170056, longitude: -73.57903745550924 },
       { latitude: 45.4971677836167, longitude: -73.57954322525673 },
       { latitude: 45.49683156812768, longitude: -73.5788507530604 },
-      { latitude: 45.497373147743474, longitude: -73.57834232326353 }
+      { latitude: 45.497373147743474, longitude: -73.57834232326353 },
     ],
   },
   {
@@ -30,7 +30,7 @@ export const buildings: Building[] = [
       { latitude: 45.49551994498565, longitude: -73.57920062907395 },
       { latitude: 45.49535857941148, longitude: -73.5793643521431 },
       { latitude: 45.49501011165603, longitude: -73.5787600651902 },
-      { latitude: 45.495193735650766, longitude: -73.57853184516286 }
+      { latitude: 45.495193735650766, longitude: -73.57853184516286 },
     ],
   },
   {
@@ -41,7 +41,7 @@ export const buildings: Building[] = [
       { latitude: 45.49586446758388, longitude: -73.57849838295294 },
       { latitude: 45.49561182833434, longitude: -73.57876050493525 },
       { latitude: 45.49524664777955, longitude: -73.57790860849278 },
-      { latitude: 45.495832313560506, longitude: -73.57726313311137},
+      { latitude: 45.495832313560506, longitude: -73.57726313311137 },
       { latitude: 45.49604131438413, longitude: -73.57771529353083 },
     ],
   },
@@ -52,7 +52,7 @@ export const buildings: Building[] = [
       { latitude: 45.494697617359805, longitude: -73.57803821546234 },
       { latitude: 45.493842086826234, longitude: -73.57907383173514 },
       { latitude: 45.493637259181746, longitude: -73.57873392697057 },
-      { latitude: 45.49445099181303, longitude: -73.57761880079917 }
+      { latitude: 45.49445099181303, longitude: -73.57761880079917 },
     ],
   },
   {
@@ -64,7 +64,7 @@ export const buildings: Building[] = [
       { latitude: 45.49637079806912, longitude: -73.57958608238289 },
       { latitude: 45.49627087833015, longitude: -73.57967517487786 },
       { latitude: 45.496166101191235, longitude: -73.57944749405738 },
-      { latitude: 45.49638328802403, longitude: -73.57923664181928 }
+      { latitude: 45.49638328802403, longitude: -73.57923664181928 },
     ],
   },
   {
@@ -93,16 +93,6 @@ export const buildings: Building[] = [
   },
   {
     campus: Campus.LOY,
-    name: "Vanier Library Building",
-    coordinates: [
-      { latitude: 45.45953646015888, longitude: -73.63909824014291 },
-      { latitude: 45.45900914646988, longitude: -73.63947855461015 },
-      { latitude: 45.458618310788864, longitude: -73.63834203346968 },
-      { latitude: 45.459074285486665, longitude: -73.63793518543496 },
-    ],
-  },
-  {
-    campus: Campus.LOY,
     name: "Hingston Hall C Building",
     coordinates: [
       { latitude: 45.459906138169075, longitude: -73.64203607306884 },
@@ -125,7 +115,7 @@ export const buildings: Building[] = [
     campus: Campus.LOY,
     name: "Hingston Hall A Building",
     coordinates: [
-      { latitude: 45.45969784997463, longitude:  -73.64136123390169 },
+      { latitude: 45.45969784997463, longitude: -73.64136123390169 },
       { latitude: 45.45942894983393, longitude: -73.64155628153311 },
       { latitude: 45.45925345641895, longitude: -73.641076060813 },
       { latitude: 45.45950631772512, longitude: -73.64087697771338 },
@@ -139,6 +129,115 @@ export const buildings: Building[] = [
       { latitude: 45.45879999060788, longitude: -73.64086057116744 },
       { latitude: 45.45866366275162, longitude: -73.6404718531157 },
       { latitude: 45.459065315920746, longitude: -73.6401646404619 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Richard J. Renaud Science Complex Building",
+    coordinates: [
+      { latitude: 45.45832821103611, longitude: -73.64141343267241 },
+      { latitude: 45.45827742437653, longitude: -73.64128710966807 },
+      { latitude: 45.458215831983296, longitude: -73.64133486592581 },
+      { latitude: 45.45818557604618, longitude: -73.64126400180143 },
+      { latitude: 45.45825797415436, longitude: -73.64120084029926 },
+      { latitude: 45.458193140032016, longitude: -73.64103908523273 },
+      { latitude: 45.458340097268966, longitude: -73.6409235458995 },
+      { latitude: 45.45831524423375, longitude: -73.64086500597067 },
+      { latitude: 45.45752534422485, longitude: -73.64146889156062 },
+      { latitude: 45.45720981336176, longitude: -73.64065395413022 },
+      { latitude: 45.45698829235157, longitude: -73.64082957391673 },
+      { latitude: 45.45744213934359, longitude: -73.64200037251645 },
+      { latitude: 45.45764312756321, longitude: -73.64184786058948 },
+      { latitude: 45.457677706108356, longitude: -73.6419233462872 },
+      { latitude: 45.45832604990376, longitude: -73.64141343269652 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Perform Centre Building",
+    coordinates: [
+      { latitude: 45.457284244031285, longitude: -73.63763317156965 },
+      { latitude: 45.45701819500725, longitude: -73.63783558975122 },
+      { latitude: 45.45668459808416, longitude: -73.63699447342395 },
+      { latitude: 45.45695340567661, longitude: -73.636780263892 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Centre for Structural and Functional Genomics Building",
+    coordinates: [
+      { latitude: 45.457171556137446, longitude: -73.64057071367107 },
+      { latitude: 45.456952508899235, longitude: -73.64074464211575 },
+      { latitude: 45.45680693906143, longitude: -73.64034342081722 },
+      { latitude: 45.4570440097478, longitude: -73.64017344529175 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Vanier Library",
+    coordinates: [
+      { latitude: 45.459485372131994, longitude: -73.63913679975713 },
+      { latitude: 45.45935160556695, longitude: -73.63923974928578 },
+      { latitude: 45.45932201115127, longitude: -73.63918067988408 },
+      { latitude: 45.45926755738584, longitude: -73.63921949691948 },
+      { latitude: 45.45930425449441, longitude: -73.63932413414534 },
+      { latitude: 45.45921783868454, longitude: -73.6393308849341 },
+      { latitude: 45.45921783868454, longitude: -73.63933594802567 },
+      { latitude: 45.459112482243995, longitude: -73.63941864518803 },
+      { latitude: 45.45899292020223, longitude: -73.63912667357397 },
+      { latitude: 45.45908170392085, longitude: -73.63900515937621 },
+      { latitude: 45.45903672018755, longitude: -73.63887858208687 },
+      { latitude: 45.45884257945327, longitude: -73.6390254117425 },
+      { latitude: 45.45861884326248, longitude: -73.63841615304815 },
+      { latitude: 45.4588567848922, longitude: -73.63820012780769 },
+      { latitude: 45.458812984767114, longitude: -73.63807017512399 },
+      { latitude: 45.45910301198917, longitude: -73.63784908679193 },
+      { latitude: 45.459213103564494, longitude: -73.63813261992004 },
+      { latitude: 45.459134974081564, longitude: -73.6382018155049 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Concordia Stadium",
+    coordinates: [
+      { latitude: 45.45880466836896, longitude: -73.63716203195673 },
+      { latitude: 45.45782822337074, longitude: -73.63834462554809 },
+      { latitude: 45.45742531638041, longitude: -73.63756073493894 },
+      { latitude: 45.457624400194284, longitude: -73.63731070086536 },
+      { latitude: 45.45753433855599, longitude: -73.6371485166014 },
+      { latitude: 45.4580652261379, longitude: -73.6365335679339 },
+      { latitude: 45.45816440414564, longitude: -73.6366925663031 },
+      { latitude: 45.458387150799744, longitude: -73.6364310451045 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Loyola Jesuit Hall and Conference Centre Building",
+    coordinates: [
+      { latitude: 45.45862022823182, longitude: -73.64002729351685 },
+      { latitude: 45.45851645293705, longitude: -73.64011576618299 },
+      { latitude: 45.45839267330936, longitude: -73.63974897325465 },
+      { latitude: 45.45861884326248, longitude: -73.63954989015503 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Loyola Administration Building",
+    coordinates: [
+      { latitude: 45.45861884326248, longitude: -73.63954989015503 },
+      { latitude: 45.45839267330936, longitude: -73.63974897325465 },
+      { latitude: 45.458255329639066, longitude: -73.63945350094436 },
+      { latitude: 45.45848550455252, longitude: -73.63925441784474 },
+    ],
+  },
+  {
+    campus: Campus.LOY,
+    name: "Loyola Hall Building",
+    coordinates: [
+      { latitude: 45.45848550455252, longitude: -73.63925441784474 },
+      { latitude: 45.458255329639066, longitude: -73.63945350094436 },
+      { latitude: 45.458215831983296, longitude: -73.63933486592581 },
+      { latitude: 45.45826751828954, longitude: -73.63923686418134 },
     ],
   },
 ];
